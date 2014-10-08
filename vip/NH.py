63c83ebfb22585522694151d9c7f3c0a90250f73
Michael Lambright <michael.g.lambright@gmail.com>
2014-10-08 00:11:31 -0400

    Fix NH

--- vip/NH.py
+++ vip/NH.py
@@ -156,23 +156,20 @@ def getValues(row):
 def getOption(row, soup, name):
     nameBase = 'ctl00$MainContentPlaceHolder$'
     city = str(row['vf_reg_cass_city'])
-    print city
     options = soup.find('select',
                         {'name': nameBase + name}).find_all('option')
     optionList = []
     maximum = 0
     for option in options:
-        text = str(option.text)
+        text = str(option.text.upper())
         value = str(option.get('value'))
         if city == text:
-            print text
             return value
         ratio = Levenshtein.ratio(city, text)
         maximum = max(maximum, ratio)
         optionList.append((ratio, value))
     for item in optionList:
         if item[0] == maximum:
-            print item[1]
             return item[1]
 
 
@@ -220,7 +217,6 @@ def query(browser, townValue, fname, lname, date, security):
     browser.select_form("aspnetForm")
     browser.set_all_readonly(False)
     baseName = 'ctl00$MainContentPlaceHolder$'
-    print townValue, fname, lname, date
     browser[baseName + 'ddlCity'] = [townValue]
     browser[baseName + 'txtFirstName'] = fname
     browser[baseName + 'txtLastName'] = lname
